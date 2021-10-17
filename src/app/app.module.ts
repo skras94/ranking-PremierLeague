104aa9b85c458a5cbd6b0ea8342cbddc3d2b3fe5
skras94 <sebastian.kranczuk@gmail.com>
2021-10-17 12:54:46 +0200

    division of app-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { InfoComponent } from './info/info.component';
+import { FooterComponent } from './footer/footer.component';
+import { RankingComponent } from './ranking/ranking.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavComponent,
+    InfoComponent,
+    FooterComponent,
+    RankingComponent,
   ],
   imports: [
     BrowserModule,
